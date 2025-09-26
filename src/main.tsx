410131f0689cdc1e4b946437668ed6d959f38920
Mrbonano0003 <mrbonano003@gmail.com>
2025-09-25 21:43:01 -0700

    Initial Character Forge

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";        // <— no .tsx here
+import "./style.css";
 
-import './index.css'
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
